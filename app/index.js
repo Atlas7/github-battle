c8b5971152425d890f03c619cbc1c57108bda5f8
atlas7 <johnnychan0302@gmail.com>
2016-06-26 11:36:31 +0100

    add FIRST comments

--- app/index.js
+++ app/index.js
@@ -1,12 +1,69 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Hello = React.createClass({
-  render: function () {
+var USER_DATA = {
+  name: 'Johnny Chan',
+  username: 'atlas7',
+  image: 'https://pbs.twimg.com/profile_images/531044251199475712/2g7V13wm.jpeg'
+};
+
+
+// function(someData) = someView
+// fn(d)=v
+
+/*
+  Focused
+  Independent
+  Reusable
+  Small
+  Testable
+*/
+
+
+var ProfilePic = React.createClass({
+  render: function() {
     return (
-      <div>Hello world!!!!</div>
+      <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
     )
   }
 });
 
-ReactDOM.render(<Hello />, document.getElementById('app'));
+var ProfileLink = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    )
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function() {
+    return (
+      <div>
+        {this.props.name}
+      </div>
+    )
+  }
+});
+
+var Avatar = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    )
+  }
+});
+
+
+ReactDOM.render(
+  <Avatar user={USER_DATA} />,
+  document.getElementById('app')
+);
