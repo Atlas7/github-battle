3b9b3b85e5c2339db20d126186acf01f1f02f280
atlas7 <johnnychan0302@gmail.com>
2016-07-01 22:01:01 +0100

    add PropTypes

--- app/components/Prompt.js
+++ app/components/Prompt.js
@@ -1,7 +1,15 @@
 var React = require('react')
+var PropTypes = React.PropTypes
 var transparentBg = require('../styles').transparentBg
 
+
 var Prompt = React.createClass({
+  propTypes: {
+    header: PropTypes.string.isRequired,
+    onSubmitUser: PropTypes.func.isRequired,
+    onUpdateUser: PropTypes.func.isRequired,
+    username: PropTypes.string.isRequired
+  },
   render: function () {
     return (
       <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
